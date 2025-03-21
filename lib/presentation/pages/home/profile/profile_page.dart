b0ce1f54e4ca5a5f437a6e0db900d1b69b43eab9
yukimiyake0607 <yukimiyake38@gmail.com>
2025-03-21 18:10:28 +0900

    add: プロフィール画面にログアウト機能を追加

--- lib/presentation/pages/home/profile/profile_page.dart
+++ lib/presentation/pages/home/profile/profile_page.dart
@@ -1,17 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:todo_app/presentation/core/appbar/custom_appbar.dart';
+import 'package:todo_app/presentation/providers/auth/auth_actions_provider.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends ConsumerWidget {
   const ProfilePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBar(
+  Widget build(BuildContext context, WidgetRef ref) {
+    void showSignOutDialog() {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: const Text('ログアウト'),
+              content: const Text('ログアウトしてもよろしいですか？'),
+              actions: [
+                TextButton(
+                  onPressed: () => context.pop(),
+                  child: const Text('キャンセル'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    context.pop();
+                    ref.read(authActionsProvider).signOut();
+                  },
+                  child: const Text('ログアウト'),
+                ),
+              ],
+            );
+          });
+    }
+
+    return Scaffold(
+      appBar: const CustomAppBar(
         title: 'プロフィール',
       ),
       body: Center(
-        child: Text('設定画面を表示'),
+        child: IconButton(
+          onPressed: () => showSignOutDialog(),
+          icon: const Icon(Icons.logout),
+        ),
       ),
     );
   }
