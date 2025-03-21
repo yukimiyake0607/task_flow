a8d6541963e8107c4f412de82af89bb0dbe72caf
yukimiyake0607 <yukimiyake38@gmail.com>
2025-03-21 15:46:04 +0900

    udpate: 中心に配置

--- lib/presentation/pages/auth/login/forgot_password_page.dart
+++ lib/presentation/pages/auth/login/forgot_password_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_app/presentation/core/appbar/custom_appbar.dart';
 import 'package:todo_app/presentation/pages/auth/widgets/auth_button.dart';
@@ -37,40 +38,51 @@ class _ForgotPasswordPageState extends ConsumerState<ForgotPasswordPage> {
         padding: const EdgeInsets.all(16),
         child: Form(
           key: _formKey,
-          child: Column(
-            children: [
-              const Text('パスワードリセットメールを送信します。'),
-              const Text('登録したメールアドレスを入力してください。'),
-              const SizedBox(height: 32),
-              TextFormField(
-                controller: _emailController,
-                decoration: const InputDecoration(
-                  labelText: 'メールアドレス',
-                  labelStyle: TextStyle(
-                    fontSize: 14,
-                    color: Colors.grey,
-                  ),
-                  border: OutlineInputBorder(),
+          child: LayoutBuilder(builder: (context, constrains) {
+            return SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: constrains.maxHeight,
                 ),
-                keyboardType: TextInputType.emailAddress,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'メールアドレスを入力してください';
-                  }
-                  if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
-                    return '有効なメールアドレスを入力してください';
-                  }
-                  return null;
-                },
-                enabled: !isLoading,
+                child: Center(
+                  child: Column(
+                    children: [
+                      const Text('パスワードリセットメールを送信します。'),
+                      const Text('登録したメールアドレスを入力してください。'),
+                      const SizedBox(height: 32),
+                      TextFormField(
+                        controller: _emailController,
+                        decoration: const InputDecoration(
+                          labelText: 'メールアドレス',
+                          labelStyle: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey,
+                          ),
+                          border: OutlineInputBorder(),
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'メールアドレスを入力してください';
+                          }
+                          if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                            return '有効なメールアドレスを入力してください';
+                          }
+                          return null;
+                        },
+                        enabled: !isLoading,
+                      ),
+                      const SizedBox(height: 32),
+                      AuthButton(
+                        buttonTitle: 'リセットメールを送信',
+                        onPressed: isLoading ? null : _resetPassword,
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              const SizedBox(height: 32),
-              AuthButton(
-                buttonTitle: 'リセットメールを送信',
-                onPressed: isLoading ? null : _resetPassword,
-              ),
-            ],
-          ),
+            );
+          }),
         ),
       ),
     );
