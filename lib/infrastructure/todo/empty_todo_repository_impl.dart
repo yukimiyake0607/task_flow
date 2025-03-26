7313aca1370ca4773259c2d229120821faf17430
yukimiyake0607 <yukimiyake38@gmail.com>
2025-03-26 21:42:43 +0900

    update: currentUserIdProviderをStreamProviderに変更して、認証を確実に待ってから取得を開始

---
+++ lib/infrastructure/todo/empty_todo_repository_impl.dart
@@ -0,0 +1,25 @@
+import 'package:todo_app/domain/entities/todo_model.dart';
+import 'package:todo_app/domain/repositories/interfaces/todo_repository_interface.dart';
+
+class EmptyTodoRepositoryImpl implements ITodoRepository {
+  @override
+  Stream<List<TodoModel>> getAllTodos() => Stream.value([]);
+
+  @override
+  Stream<List<TodoModel>> getCompletedTodo() => Stream.value([]);
+
+  @override
+  Stream<List<TodoModel>> getInCompletedTodo() => Stream.value([]);
+
+  @override
+  Future<String> addTodo(TodoModel todoModel) async => '';
+
+  @override
+  Future<void> updateTodo(TodoModel todoModel) async {}
+
+  @override
+  Future<void> deleteTodo(String id) async {}
+
+  @override
+  Future<void> setTodoCompleted(TodoModel todoModel, bool isCompleted) async {}
+}
