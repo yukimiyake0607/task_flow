98d4949a5efbfc9ac563f3c3f10388cec0311b7c
yukimiyake0607 <yukimiyake38@gmail.com>
2025-04-01 17:40:31 +0900

    refactor: themeを切り出し（タイトルのテキストスタイルを作成）

--- lib/presentation/core/theme/todo_theme.dart
+++ lib/presentation/core/theme/todo_theme.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
+const h1TitleStyle = TextStyle(
+  fontSize: 22,
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+);
+
+const h2TitleStyle = TextStyle(
+  fontSize: 18,
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+);
+
 const todoMainColor = Color(0xFF4A6FFF);
 const todoCardBorderColor = Color(0xFFFEF3C7);
 
