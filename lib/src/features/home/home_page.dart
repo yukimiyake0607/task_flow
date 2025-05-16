8332bec2271c72229b228eae14a22ce7254a22ff
yukimiyake0607 <yukimiyake38@gmail.com>
2025-05-16 12:34:51 +0900

    refactor: completedの保管位場所変更

--- lib/src/features/home/home_page.dart
+++ lib/src/features/home/home_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_app/src/common_widgets/bottom_navigation/bottom_navigation_provider.dart';
 import 'package:todo_app/src/common_widgets/bottom_navigation/bottom_navigation.dart';
-import 'package:todo_app/src/features/completed/presentation/completed_page.dart';
+import 'package:todo_app/src/features/todo/presentation/completed/completed_page.dart';
 import 'package:todo_app/src/features/profile/presentation/profile_page.dart';
 import 'package:todo_app/src/features/todo/todo/todo_page.dart';
 
