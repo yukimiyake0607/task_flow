89e9ada4c34122b336d5538f7c303a462c5ed132
yukimiyake0607 <yukimiyake38@gmail.com>
2025-05-17 20:24:26 +0900

    update: 削除ボタンを削除して、スワイプすることでイベントが発火するように変更

--- lib/src/features/todo/presentation/todo_card.dart
+++ lib/src/features/todo/presentation/todo_card.dart
@@ -28,74 +28,80 @@ class _TodoCardState extends ConsumerState<TodoCard> {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        if (!_isChecked) {
-          showDialog(
-            context: context,
-            builder: (context) {
-              return TodoDialog(
-                buttonTitle: '編集',
-                todoModel: widget.todoModel,
-              );
-            },
-          );
-        }
-        return;
+    return Dismissible(
+      key: ValueKey(widget.todoModel.id),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) async {
+        await ref
+            .read(todoControllerProvider.notifier)
+            .deleteTodo(widget.todoModel.id);
       },
-      child: Container(
-        decoration: BoxDecoration(
-          color: _isChecked == false
-              ? Colors.white
-              : completedCardColor.withAlpha(13),
-          border: Border(
-            bottom: BorderSide(
-              color: Colors.grey.shade100,
-              width: 1,
-            ),
-          ),
+      background: Container(
+        color: Colors.red,
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 20.0),
+        child: const Icon(
+          Icons.delete,
+          color: Colors.white,
         ),
-        child: Row(
-          children: [
-            Checkbox(
-              value: _isChecked,
-              onChanged: (newValue) async {
-                setState(() {
-                  _isChecked = newValue ?? false;
-                });
-
-                await Future.delayed(const Duration(milliseconds: 500));
-
-                if (!mounted) return;
-
-                await ref
-                    .read(todoControllerProvider.notifier)
-                    .toggleTodo(widget.todoModel);
+      ),
+      child: GestureDetector(
+        onTap: () {
+          if (!_isChecked) {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return TodoDialog(
+                  buttonTitle: '編集',
+                  todoModel: widget.todoModel,
+                );
               },
-              side: BorderSide(color: Colors.grey.shade500, width: 2),
-            ),
-            Text(
-              widget.todoModel.todoTitle,
-              style: TextStyle(
-                decoration: widget.todoModel.isCompleted == false
-                    ? null
-                    : TextDecoration.lineThrough,
+            );
+          }
+          return;
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            color: _isChecked == false
+                ? Colors.white
+                : completedCardColor.withAlpha(13),
+            border: Border(
+              bottom: BorderSide(
+                color: Colors.grey.shade100,
+                width: 1,
               ),
             ),
+          ),
+          child: Row(
+            children: [
+              Checkbox(
+                value: _isChecked,
+                onChanged: (newValue) async {
+                  setState(() {
+                    _isChecked = newValue ?? false;
+                  });
 
-            const Spacer(),
+                  await Future.delayed(const Duration(milliseconds: 500));
 
-            // 削除ボタン：両方
-            IconButton(
-              onPressed: () async {
-                await ref
-                    .read(todoControllerProvider.notifier)
-                    .deleteTodo(widget.todoModel.id);
-              },
-              icon: const Icon(Icons.delete_outlined),
-              visualDensity: VisualDensity.compact,
-            ),
-          ],
+                  if (!mounted) return;
+
+                  await ref
+                      .read(todoControllerProvider.notifier)
+                      .toggleTodo(widget.todoModel);
+                },
+                side: BorderSide(color: Colors.grey.shade500, width: 2),
+              ),
+              Text(
+                widget.todoModel.todoTitle,
+                style: TextStyle(
+                  decoration: widget.todoModel.isCompleted == false
+                      ? null
+                      : TextDecoration.lineThrough,
+                ),
+              ),
+              const Spacer(),
+            ],
+          ),
         ),
       ),
     );
